8bea8c0a788f5b0a869e2f98aea73953a007c63b
Stiven hoxha <stivenhoxha007@gmail.com>
2025-11-29 20:08:28 +0100

    array di oggetti statici momentanea

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-//# OPERAZIONI DI PREPARAZIONE
+// #OPERAZIONI DI PREPARAZIONE
 
 // 1 --> Recupero gli elementi di interesse dalla pagina
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const chatBox = document.querySelector('.chat-box');
 
+// 2 --> Preparazione dei messaggi iniziali - un array di oggetti (lista di oggetti)
+const messages = [
+
+    {
+        type: 'sent',
+        text: 'Ciao come va?',
+        time: '24/11/25 20:04:12'
+    },
+    {
+        type: 'received',
+        text: 'Bene tu?',
+        time: '24/11/25 20:20:15'
+    }
+
+];
 

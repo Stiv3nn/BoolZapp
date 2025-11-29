463fa17efb48cfc9055a32a0e138537ab74097b1
Stiven hoxha <stivenhoxha007@gmail.com>
2025-11-29 19:57:47 +0100

    collegamento di js e recupero degli elementi di interesse della pagina

---
+++ script.js
@@ -0,0 +1,8 @@
+//# OPERAZIONI DI PREPARAZIONE
+
+// 1 --> Recupero gli elementi di interesse dalla pagina
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const chatBox = document.querySelector('.chat-box');
+
+

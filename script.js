b875248fb85fad4dc0e73d8751fd9a458f81a1b2
Stiven hoxha <stivenhoxha007@gmail.com>
2025-12-03 21:00:24 +0100

    creazione della funzione per aggiungere un messaggio, scroll automatico, click dell'invio + css

--- script.js
+++ script.js
@@ -52,18 +52,21 @@ button.addEventListener('click', function () {
     const insertedText = input.value.trim();
 
     // 7 --> creo un nuovo messaggio
-    const newMessage = {
-        type: 'sent',
-        text: insertedText,
-        time: new Date().toLocaleDateString()
-    }
+    // const newMessage = {
+    //     type: 'sent',
+    //     text: insertedText,
+    //     time: new Date().toLocaleDateString()
+    // }
 
     // 8 --> controllare che non sia vuoto
     // se non c'è testo, annulla tutto (istruzioni condizionali)
-    if (insertedText === '') return
+    // if (insertedText === '') return
 
     // 9 --> aggiungo questo messaggio alla fine della lista (array) dei messages
-    messages.push(newMessage);
+    // messages.push(newMessage);
+
+    // 15 --> aggiungo il messaggio in pagina
+    addMessage('sent', insertedText); // 17 --> dobbiamo darli il tipo e il testo --> cioè, (domanda) --> se io sono al click del buttone, che tipo sarà? --> sent
 
     // 3 --> Svuoto la chat tramite innerHTML che è uguale a ''
     // chatBox.innerHTML = '';
@@ -82,13 +85,24 @@ button.addEventListener('click', function () {
     // `
     // }
 
-    // 12 --> mostro i messaggi in pagina
-    showMessages();
+    // 8 --> controllare che non sia vuoto
+    // se non c'è testo, annulla tutto (istruzioni condizionali)
+    if (insertedText === '') return
+
 
     // 10 --> svuoto la casella di testo --> input
     input.value = '';
     // 11 --> riporto il focus nella casella di testo dopo l'invio del messaggio
     input.focus();
+
+    // 16 --> scorro in automatico alla fine dei box
+    chatBox.scrollTop = chatBox.scrollHeight;
+})
+
+// 17 --> Al pressione del tasto invio --> non è pià-ù un click ma bisgona mettere key o keydown per l'invio
+input.addEventListener('keydown', function (event){
+    // 18 --> controllo se il tasto cliccato è invio
+    if(event.key === 'Enter') sendMessage()
 })
 
 
@@ -103,15 +117,35 @@ function showMessages() {
     // 4 --> Per ciascuno dei messaggi.....(ciclo for of)
     for (const message of messages) { // per ognuno dei messaggi creami una variabile messaggio
         chatBox.innerHTML += `
-    <div class="chat-row ${message.type}">
-            <div class="chat-message">
-                <p>${message.text}</p>
-                <time datetime="${message.time}">
-                    ${message.time}
-                </time>
-            </div>
+        <div class="chat-row ${message.type}">
+        <div class="chat-message">
+        <p>${message.text}</p>
+        <time datetime="${message.time}">
+        ${message.time}
+        </time>
         </div>
-    `
+        </div>
+        `
     }
 }
+
+
+// 14 --> funzione per aggiungere un messaggio
+function addMessage(messageType, messageText) { // 16 --> aggiungo i paraemtri per recuperare info dall'esterno
+
+    // 7 --> creo un nuovo messaggio
+    const newMessage = {
+        type: messageType,
+        text: messageText,
+        time: new Date().toLocaleDateString()
+    }
+
+
+    // 9 --> aggiungo questo messaggio alla fine della lista (array) dei messages
+    messages.push(newMessage);
+
+    // 12 --> mostro i messaggi in pagina
+    showMessages();
+}
+
 
